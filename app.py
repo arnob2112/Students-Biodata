8887866f7b3d0e945e62bc7f8e599a07f3133d0e
Md Ehshanul Haque <ehshanulhq@gmail.com>
2022-09-27 18:01:22 +0600

    Testing 2nd time Herokuapp

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_restful import Api
 from Resources import ReceiveInfo, GetInfo
+import sys
+import logging
 
 app = Flask(__name__)
 app.secret_key = "Arnob"
@@ -14,6 +16,9 @@ def home():
 
 api.add_resource(ReceiveInfo, "/form")
 api.add_resource(GetInfo, "/showinfo")
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     app.run(debug=True)
