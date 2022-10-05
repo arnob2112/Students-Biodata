104392bd295ef5a2c1850891f9cdd2c04709a4c4
Md Ehshanul Haque <ehshanulhq@gmail.com>
2022-10-05 21:41:47 +0600

    For Steamlit

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ api.add_resource(GetInfo, "/showinfo")
 api.add_resource(Profile, "/<string:name>")
 api.add_resource(Show, "/show")
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
