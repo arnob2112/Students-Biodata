2bee2c05e9c97fb42aeab4be5fa1660aa99519a2
Md Ehshanul Haque <ehshanulhq@gmail.com>
2023-09-04 07:18:06 +0600

    only teacher can post notice

--- resources/notice_board.py
+++ resources/notice_board.py
@@ -10,25 +10,24 @@ from models.notice_board import NoticeBoard
 class Notice(Resource):
 
     def get(self):
-        if current_user.is_authenticated:
-            teacher_name = " ".join(Students.query.with_entities(Students.Firstname, Students.Lastname)
-                                    .filter_by(Job="Teacher", Teacher_Username=current_user.username).first())
-        else:
-            teacher_name = None
+        # if current_user.is_authenticated:
+        #     teacher_name = " ".join(Students.query.with_entities(Students.firstname, Students.lastname)
+        #                             .filter_by(job="Teacher", teacher_username=current_user.username).first())
+        # else:
+        #     teacher_name = None
         notices = NoticeBoard.query.with_entities(NoticeBoard.notice, NoticeBoard.name, NoticeBoard.date).all()[::-1]
-        if current_user.is_authenticated:
-            student_usernames, teacher_username = Students.find_all_username(current_user.username)
-        else:
-            student_usernames = None
-            teacher_username = None
+        # if current_user.is_authenticated:
+        #     student_usernames, teacher_username = Students.find_all_username(current_user.username)
+        # else:
+        #     student_usernames = None
+        #     teacher_username = None
 
-        return make_response(render_template("notice_board.html", notices=notices, teacher_name=teacher_name,
-                                             student_usernames=student_usernames, teacher_username=teacher_username))
+        return make_response(render_template("notice_board.html", notices=notices))
 
     def post(self):
         notice = request.form.get('notice')
-        teacher_name = request.form.get('teacher_name')
+        # teacher_name = request.form.get('teacher_name')
         date = datetime.now().strftime("%I:%M %p - %d %B, %Y")
-        new_notice = NoticeBoard(name=teacher_name, notice=notice, date=date)
+        new_notice = NoticeBoard(name=current_user.username, notice=notice, date=date)
         new_notice.save_to_db()
         return redirect(url_for("notice"))
