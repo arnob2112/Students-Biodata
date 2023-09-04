76f5ffd5c8dbe83411409e87a88d70efcdf5645b
Md Ehshanul Haque <ehshanulhq@gmail.com>
2023-09-04 07:14:34 +0600

    teacher can see all profile but student can only see his own profile

--- resources/Profile.py
+++ resources/Profile.py
@@ -3,18 +3,20 @@ from flask_restful import Resource
 from flask_login import login_required, current_user
 
 from models.students import Students
+from models.teachers import Teachers
 
 
 class Profile(Resource):
     @login_required
     def get(self, username, job):
-        student = Students.find_by_username(username, job)
-        if student:
-            data = {" ".join([student[x] for x in range(0, 2)]): [student[x] for x in range(2, len(student))]}
-            student_usernames, teacher_username = Students.find_all_username(current_user.username)
-            print(data)
-            return make_response(render_template("profile.html", data=data, student_usernames=student_usernames,
-                                                 teacher_username=teacher_username))
-            # a single page of an student which includes all information
+        print('get profile')
+        if current_user.job.lower() == "teacher" or username == current_user.username and job.lower() == 'student':
+            person = Students.find_by_username(username, job) or Teachers.find_by_username(username, job)
+            if person:
+                data = {" ".join([person[x] for x in range(0, 2)]): [person[x] for x in range(2, len(person))]}
+                # student_usernames, teacher_username = Students.find_all_username(current_user.username)
+                print(data)
+                return make_response(render_template("profile.html", data=data))
+            # a single page of an person which includes all information
         else:
             return None
