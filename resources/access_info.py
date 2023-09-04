66d2add31c538c3341073023544b77a9cd591508
Md Ehshanul Haque <ehshanulhq@gmail.com>
2023-09-04 07:17:40 +0600

    automated form after sign up.

--- resources/access_info.py
+++ resources/access_info.py
@@ -6,6 +6,7 @@ import requests
 import os
 
 from models.students import Students
+from models.teachers import Teachers
 from database import db
 
 
@@ -14,12 +15,19 @@ class Home(Resource):
 
     def get(self):
         if current_user.is_authenticated:
-            student_usernames, teacher_username = Students.find_all_username(current_user.username)
-        else:
-            student_usernames = None
-            teacher_username = None
-        return make_response(render_template("home.html",
-                                             student_usernames=student_usernames, teacher_username=teacher_username))
+            print("home get")
+            if Students.query.filter_by(username=current_user.username).first() is None and Teachers.query.filter_by(
+                    username=current_user.username).first() is None:
+                print("home get form")
+                return make_response(render_template("form.html", job=current_user.job,
+                                                     student_usernames=None, teacher_username=None))
+
+            # student_usernames, teacher_username = Students.find_all_username(current_user.username) # need to chech usage
+        # else:
+        #     student_usernames = None
+        #     teacher_username = None
+        return make_response(render_template("home.html"))
+                                             # student_usernames=student_usernames, teacher_username=teacher_username))
 
 
 class ReceiveInfo(Resource):
@@ -27,13 +35,13 @@ class ReceiveInfo(Resource):
 
     def get(self, job):
         print("get", job)
-        if current_user.is_authenticated:
-            student_usernames, teacher_username = Students.find_all_username(current_user.username)
-        else:
-            student_usernames = None
-            teacher_username = None
-        return make_response(render_template("form.html", job=job,
-                                             student_usernames=student_usernames, teacher_username=teacher_username))
+        # if current_user.is_authenticated:
+        #     student_usernames, teacher_username = Students.find_all_username(current_user.username)
+        # else:
+        #     student_usernames = None
+        #     teacher_username = None
+        return make_response(render_template("form.html", job=job))
+                                             # student_usernames=student_usernames, teacher_username=teacher_username))
 
     def post(self, job):
         print(job)
@@ -41,8 +49,8 @@ class ReceiveInfo(Resource):
         # initializing put method
         if data['work'] == 'Update':
             info = Students.find_by_username(data['username'], data['job'])
-            student_usernames, teacher_username = Students.find_all_username(current_user.username)
-            return make_response(render_template("update_form.html", data=info, student_usernames=student_usernames))
+            # student_usernames, teacher_username = Students.find_all_username(current_user.username)
+            return make_response(render_template("update_form.html", data=info))
 
         elif data['work'] == 'init update':
             if request.files['Picture']:
@@ -53,12 +61,11 @@ class ReceiveInfo(Resource):
             requests.put(request.url, params=data, files={'Picture': request.files['Picture']})
 
             students = Students.get_all_students(current_user.username)
-            student_usernames, teacher_username = Students.find_all_username(current_user.username)
+            # student_usernames, teacher_username = Students.find_all_username(current_user.username)
 
             return make_response(
-                render_template("showinfo.html", students=students, teacher_username=teacher_username,
-                                student_usernames=student_usernames,
-                                message="{} {} has updated.".format(data['Firstname'], data['Lastname'])))
+                render_template("showinfo.html", students=students,
+                                message="{} {} has updated.".format(data['firstname'], data['Lastname'])))
 
         # initializing delete method
         elif data["work"] == 'Delete':
@@ -66,45 +73,50 @@ class ReceiveInfo(Resource):
             requests.delete(request.url, params=data)
 
             all_students = Students.get_all_students(current_user.username)
-            student_usernames, teacher_username = Students.find_all_username(current_user.username)
+            # student_usernames, teacher_username = Students.find_all_username(current_user.username)
             return make_response(
                 render_template("showinfo.html", students=all_students,
-                                student_usernames=student_usernames, teacher_username=teacher_username,
                                 message="{} has deleted.".format(name)))
 
         else:
             if current_user.is_authenticated:
-                teacher_username = current_user.username
+                teacher_username = None  # need to be fixed
             else:
                 teacher_username = data['teacher_username']
-            username = Students.generate_username(data['firstname'], teacher_username)
-            new_student = Students(firstname=data['firstname'], lastname=data['lastname'], college=data['college'],
-                                   age=data['age'], gender=data.get('gender'), religion=data.get('religion'),
-                                   contact_number=data['number'], fb_url=data['fb_url'], job=job,
-                                   image_path=None, username=username,
-                                   teacher_username=teacher_username)
-
-            # new_student.save_to_db()
+            # username = Students.generate_username(data['firstname'], teacher_username)
 
             # saving picture in pictures folder and path in database
-            image_path = Students.find_picture(username)
+            image_path = Students.find_picture(current_user.username)
             uploaded_img = request.files['picture']
             uploaded_img.save(image_path)
 
-            student = Students.query.filter_by(Username=username).first()
-            student.Image_Path = image_path
+            if current_user.job.lower() == 'student':
+                new_student = Students(firstname=data['firstname'], lastname=data['lastname'], college=data['college'],
+                                       age=data['age'], gender=data.get('gender'), religion=data.get('religion'),
+                                       contact_number=data['number'], fb_url=data['fb_url'], job=job,
+                                       image_path=image_path, username=current_user.username,
+                                       teacher_username=teacher_username)
+
+                new_student.save_to_db()
+            else:
+                new_teacher = Teachers(firstname=data['firstname'], lastname=data['lastname'], college=data['college'],
+                                       age=data['age'], gender=data.get('gender'), religion=data.get('religion'),
+                                       contact_number=data['number'], fb_url=data['fb_url'], job=job,
+                                       image_path=image_path, username=current_user.username, )
+                new_teacher.save_to_db()
+
+            # student = Students.query.filter_by(username=current_user.username).first()
+            # student.image_path = image_path
             # student.save_to_db()
 
-            student_usernames, teacher_username = Students.find_all_username(teacher_username)
+            # student_usernames, teacher_username = Students.find_all_username(teacher_username)
             return make_response(render_template("uploaded.html", name="{} {}".format(data['firstname'],
-                                                                                      data['lastname'],
-                                                                                      student_usernames
-                                                                                      =student_usernames)))
+                                                                                      data['lastname'],)))
 
     def put(self):
         data = dict(request.args)
 
-        image_path = Students.find_picture(data['Username'])
+        image_path = Students.find_picture(data['username'])
         if data['Picture'] == "True":
             uploaded_img = request.files['Picture']
             uploaded_img.save(image_path)
@@ -112,8 +124,8 @@ class ReceiveInfo(Resource):
 
         # connection = sqlite3.connect("All Information.db")
         # cursor = connection.cursor()
-        # update_query = "UPDATE {} SET Firstname=?, LastName=?, College=?, Age=?, Gender=?, Religion=?," \
-        #                "Contact_Number=?, FB_URL=?, Job=? WHERE Username=?".format(self.TABLE_NAME)
+        # update_query = "UPDATE {} SET firstname=?, LastName=?, College=?, Age=?, Gender=?, Religion=?," \
+        #                "contact_number=?, fb_url=?, job=? WHERE username=?".format(self.TABLE_NAME)
         # cursor.execute(update_query, (data['firstname'], data['lastname'], data['college'], data['age'], data['gender'],
         #                               data['religion'], data['number'], data['fb_url'], data['job'], data['username']))
         # connection.commit()
@@ -121,7 +133,7 @@ class ReceiveInfo(Resource):
 
         data.pop('work')
         data.pop('Picture')
-        Students.query.filter_by(Username=data['Username']).update(data)
+        Students.query.filter_by(username=data['username']).update(data)
         db.session.commit()
 
         return None
@@ -131,12 +143,12 @@ class ReceiveInfo(Resource):
 
         # connection = sqlite3.connect("../All Information.db")
         # cursor = connection.cursor()
-        # delete_query = "DELETE FROM {} WHERE Username=?".format(self.TABLE_NAME)
+        # delete_query = "DELETE FROM {} WHERE username=?".format(self.TABLE_NAME)
         # cursor.execute(delete_query, (data['username'],))
         # connection.commit()
         # connection.close()
 
-        delete_student = Students.query.filter_by(Username=data['username']).first()
+        delete_student = Students.query.filter_by(username=data['username']).first()
         delete_student.delete_from_db()
 
         os.remove(Students.find_picture(data['username']))
@@ -150,10 +162,9 @@ class GetInfo(Resource):
         teacher_username = current_user.username
         students = Students.get_all_students(teacher_username)
         # teacher, students = Students.divide(all_persons)
-        student_usernames, teacher_username = Students.find_all_username(teacher_username)
+        # student_usernames, teacher_username = Students.find_all_username(teacher_username)
         return make_response(
-            render_template("showinfo.html", students=students,
-                            student_usernames=student_usernames, teacher_username=teacher_username))
+            render_template("showinfo.html", students=students))
 
     # return make_response(render_template("login.html"))
     # @login_required
@@ -190,11 +201,10 @@ class Show(Resource):
     def get(self):
         all_persons = Students.get_all_students("ehshan")
         teacher, students = Students.divide(all_persons)
-        if current_user.is_authenticated:
-            student_usernames, teacher_username = Students.find_all_username(current_user.username)
-        else:
-            student_usernames = None
-            teacher_username = None
+        # if current_user.is_authenticated:
+        #     student_usernames, teacher_username = Students.find_all_username(current_user.username)
+        # else:
+        #     student_usernames = None
+        #     teacher_username = None
         return make_response(
-            render_template("showinfo.html", teacher=teacher, students=students, data=all_persons,
-                            student_usernames=student_usernames, teacher_username=teacher_username))
+            render_template("showinfo.html", teacher=teacher, students=students, data=all_persons))
